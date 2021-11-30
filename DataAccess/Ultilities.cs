830e1fe219beb952dd42de69b2849cafa36d65f5
haclamthien <haclamthien@gmail.com>
2021-11-30 14:56:55 +0700

    add connect string

---
+++ DataAccess/Ultilities.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class Ultilities
+    {
+        private static string StrName = "ConnectionStringName";
+        public static string ConnectionString = ConfigurationManager.ConnectionStrings[StrName].ConnectionString;
+        //---Job
+        //---JobDetail
+        //---TypeOfJob
+        //---Account
+
+        //public static string ...
+    }
+}
