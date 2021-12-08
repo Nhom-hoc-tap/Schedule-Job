87a14429be95f84df6c7807dfe5346025ab18531
SwankyOrcc <1911185@dlu.edu.vn>
2021-12-08 14:05:12 +0700

    Updated 2:05

--- Schedule Job/AddjobDetailFrm.Designer.cs
+++ Schedule Job/AddjobDetailFrm.Designer.cs
@@ -48,6 +48,8 @@ namespace Schedule_Job
 			this.label1 = new System.Windows.Forms.Label();
 			this.guna2TextBox4 = new Guna.UI2.WinForms.Guna2TextBox();
 			this.guna2TextBox5 = new Guna.UI2.WinForms.Guna2TextBox();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
 			this.groupBox1.SuspendLayout();
 			this.SuspendLayout();
 			// 
@@ -372,11 +374,33 @@ namespace Schedule_Job
 			this.guna2TextBox5.Size = new System.Drawing.Size(111, 36);
 			this.guna2TextBox5.TabIndex = 62;
 			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label7.Location = new System.Drawing.Point(152, 152);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(37, 17);
+			this.label7.TabIndex = 63;
+			this.label7.Text = "Phút";
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label8.Location = new System.Drawing.Point(152, 227);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(37, 17);
+			this.label8.TabIndex = 64;
+			this.label8.Text = "Phút";
+			// 
 			// AddjobDetailFrm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(544, 573);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.label7);
 			this.Controls.Add(this.guna2TextBox5);
 			this.Controls.Add(this.guna2TextBox4);
 			this.Controls.Add(this.guna2TextBox3);
@@ -422,5 +446,7 @@ namespace Schedule_Job
 		private System.Windows.Forms.Label label1;
 		private Guna.UI2.WinForms.Guna2TextBox guna2TextBox4;
 		private Guna.UI2.WinForms.Guna2TextBox guna2TextBox5;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label8;
 	}
 }
