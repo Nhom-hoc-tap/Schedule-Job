57303839e10d7c0fa7a63a6c71cf1e535dbf223b
SwankyOrcc <1911185@dlu.edu.vn>
2021-12-08 14:01:38 +0700

    Updated 2:01

--- Schedule Job/AddjobDetailFrm.Designer.cs
+++ Schedule Job/AddjobDetailFrm.Designer.cs
@@ -40,16 +40,14 @@ namespace Schedule_Job
 			this.guna2CheckBox3 = new Guna.UI2.WinForms.Guna2CheckBox();
 			this.guna2ComboBox1 = new Guna.UI2.WinForms.Guna2ComboBox();
 			this.guna2TextBox2 = new Guna.UI2.WinForms.Guna2TextBox();
-			this.guna2DateTimePicker4 = new Guna.UI2.WinForms.Guna2DateTimePicker();
-			this.guna2DateTimePicker3 = new Guna.UI2.WinForms.Guna2DateTimePicker();
 			this.guna2TextBox1 = new Guna.UI2.WinForms.Guna2TextBox();
-			this.guna2DateTimePicker2 = new Guna.UI2.WinForms.Guna2DateTimePicker();
-			this.guna2DateTimePicker1 = new Guna.UI2.WinForms.Guna2DateTimePicker();
 			this.label5 = new System.Windows.Forms.Label();
 			this.label4 = new System.Windows.Forms.Label();
 			this.label3 = new System.Windows.Forms.Label();
 			this.label2 = new System.Windows.Forms.Label();
 			this.label1 = new System.Windows.Forms.Label();
+			this.guna2TextBox4 = new Guna.UI2.WinForms.Guna2TextBox();
+			this.guna2TextBox5 = new Guna.UI2.WinForms.Guna2TextBox();
 			this.groupBox1.SuspendLayout();
 			this.SuspendLayout();
 			// 
@@ -67,20 +65,20 @@ namespace Schedule_Job
 			this.guna2TextBox3.Font = new System.Drawing.Font("Segoe UI", 9F);
 			this.guna2TextBox3.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
 			this.guna2TextBox3.HoverState.Parent = this.guna2TextBox3;
-			this.guna2TextBox3.Location = new System.Drawing.Point(425, 298);
+			this.guna2TextBox3.Location = new System.Drawing.Point(37, 299);
 			this.guna2TextBox3.Name = "guna2TextBox3";
 			this.guna2TextBox3.PasswordChar = '\0';
 			this.guna2TextBox3.PlaceholderText = "";
 			this.guna2TextBox3.SelectedText = "";
 			this.guna2TextBox3.ShadowDecoration.Parent = this.guna2TextBox3;
-			this.guna2TextBox3.Size = new System.Drawing.Size(216, 36);
+			this.guna2TextBox3.Size = new System.Drawing.Size(207, 36);
 			this.guna2TextBox3.TabIndex = 60;
 			// 
 			// label6
 			// 
 			this.label6.AutoSize = true;
 			this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label6.Location = new System.Drawing.Point(41, 356);
+			this.label6.Location = new System.Drawing.Point(33, 356);
 			this.label6.Name = "label6";
 			this.label6.Size = new System.Drawing.Size(49, 20);
 			this.label6.TabIndex = 59;
@@ -100,7 +98,7 @@ namespace Schedule_Job
 			this.btnCancel.Font = new System.Drawing.Font("Segoe UI", 9F);
 			this.btnCancel.ForeColor = System.Drawing.Color.White;
 			this.btnCancel.HoverState.Parent = this.btnCancel;
-			this.btnCancel.Location = new System.Drawing.Point(510, 498);
+			this.btnCancel.Location = new System.Drawing.Point(372, 492);
 			this.btnCancel.Name = "btnCancel";
 			this.btnCancel.ShadowDecoration.Parent = this.btnCancel;
 			this.btnCancel.Size = new System.Drawing.Size(131, 45);
@@ -121,7 +119,7 @@ namespace Schedule_Job
 			this.guna2Button2.Font = new System.Drawing.Font("Segoe UI", 9F);
 			this.guna2Button2.ForeColor = System.Drawing.Color.White;
 			this.guna2Button2.HoverState.Parent = this.guna2Button2;
-			this.guna2Button2.Location = new System.Drawing.Point(370, 498);
+			this.guna2Button2.Location = new System.Drawing.Point(232, 492);
 			this.guna2Button2.Name = "guna2Button2";
 			this.guna2Button2.ShadowDecoration.Parent = this.guna2Button2;
 			this.guna2Button2.Size = new System.Drawing.Size(131, 45);
@@ -135,9 +133,9 @@ namespace Schedule_Job
 			this.groupBox1.Controls.Add(this.guna2CheckBox2);
 			this.groupBox1.Controls.Add(this.guna2CheckBox3);
 			this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.groupBox1.Location = new System.Drawing.Point(425, 51);
+			this.groupBox1.Location = new System.Drawing.Point(287, 110);
 			this.groupBox1.Name = "groupBox1";
-			this.groupBox1.Size = new System.Drawing.Size(216, 125);
+			this.groupBox1.Size = new System.Drawing.Size(216, 143);
 			this.groupBox1.TabIndex = 56;
 			this.groupBox1.TabStop = false;
 			this.groupBox1.Text = "Trạng thái";
@@ -226,10 +224,10 @@ namespace Schedule_Job
             "Quan trọng",
             "Không quan trọng"});
 			this.guna2ComboBox1.ItemsAppearance.Parent = this.guna2ComboBox1;
-			this.guna2ComboBox1.Location = new System.Drawing.Point(160, 298);
+			this.guna2ComboBox1.Location = new System.Drawing.Point(287, 299);
 			this.guna2ComboBox1.Name = "guna2ComboBox1";
 			this.guna2ComboBox1.ShadowDecoration.Parent = this.guna2ComboBox1;
-			this.guna2ComboBox1.Size = new System.Drawing.Size(164, 36);
+			this.guna2ComboBox1.Size = new System.Drawing.Size(216, 36);
 			this.guna2ComboBox1.TabIndex = 53;
 			// 
 			// guna2TextBox2
@@ -252,46 +250,8 @@ namespace Schedule_Job
 			this.guna2TextBox2.PlaceholderText = "";
 			this.guna2TextBox2.SelectedText = "";
 			this.guna2TextBox2.ShadowDecoration.Parent = this.guna2TextBox2;
-			this.guna2TextBox2.Size = new System.Drawing.Size(370, 36);
+			this.guna2TextBox2.Size = new System.Drawing.Size(466, 36);
 			this.guna2TextBox2.TabIndex = 51;
-			// 
-			// guna2DateTimePicker4
-			// 
-			this.guna2DateTimePicker4.BorderRadius = 8;
-			this.guna2DateTimePicker4.Checked = true;
-			this.guna2DateTimePicker4.CheckedState.Parent = this.guna2DateTimePicker4;
-			this.guna2DateTimePicker4.CustomFormat = "hh:mm";
-			this.guna2DateTimePicker4.FillColor = System.Drawing.Color.White;
-			this.guna2DateTimePicker4.Font = new System.Drawing.Font("Segoe UI", 9F);
-			this.guna2DateTimePicker4.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-			this.guna2DateTimePicker4.HoverState.Parent = this.guna2DateTimePicker4;
-			this.guna2DateTimePicker4.Location = new System.Drawing.Point(273, 226);
-			this.guna2DateTimePicker4.MaxDate = new System.DateTime(9998, 12, 31, 0, 0, 0, 0);
-			this.guna2DateTimePicker4.MinDate = new System.DateTime(1753, 1, 1, 0, 0, 0, 0);
-			this.guna2DateTimePicker4.Name = "guna2DateTimePicker4";
-			this.guna2DateTimePicker4.ShadowDecoration.Parent = this.guna2DateTimePicker4;
-			this.guna2DateTimePicker4.ShowUpDown = true;
-			this.guna2DateTimePicker4.Size = new System.Drawing.Size(134, 43);
-			this.guna2DateTimePicker4.TabIndex = 50;
-			this.guna2DateTimePicker4.Value = new System.DateTime(2021, 12, 8, 8, 37, 11, 123);
-			// 
-			// guna2DateTimePicker3
-			// 
-			this.guna2DateTimePicker3.BorderRadius = 8;
-			this.guna2DateTimePicker3.Checked = true;
-			this.guna2DateTimePicker3.CheckedState.Parent = this.guna2DateTimePicker3;
-			this.guna2DateTimePicker3.FillColor = System.Drawing.Color.White;
-			this.guna2DateTimePicker3.Font = new System.Drawing.Font("Segoe UI", 9F);
-			this.guna2DateTimePicker3.Format = System.Windows.Forms.DateTimePickerFormat.Long;
-			this.guna2DateTimePicker3.HoverState.Parent = this.guna2DateTimePicker3;
-			this.guna2DateTimePicker3.Location = new System.Drawing.Point(37, 226);
-			this.guna2DateTimePicker3.MaxDate = new System.DateTime(9998, 12, 31, 0, 0, 0, 0);
-			this.guna2DateTimePicker3.MinDate = new System.DateTime(1753, 1, 1, 0, 0, 0, 0);
-			this.guna2DateTimePicker3.Name = "guna2DateTimePicker3";
-			this.guna2DateTimePicker3.ShadowDecoration.Parent = this.guna2DateTimePicker3;
-			this.guna2DateTimePicker3.Size = new System.Drawing.Size(230, 43);
-			this.guna2DateTimePicker3.TabIndex = 49;
-			this.guna2DateTimePicker3.Value = new System.DateTime(2021, 12, 8, 8, 21, 48, 501);
 			// 
 			// guna2TextBox1
 			// 
@@ -307,58 +267,20 @@ namespace Schedule_Job
 			this.guna2TextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
 			this.guna2TextBox1.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
 			this.guna2TextBox1.HoverState.Parent = this.guna2TextBox1;
-			this.guna2TextBox1.Location = new System.Drawing.Point(45, 379);
+			this.guna2TextBox1.Location = new System.Drawing.Point(35, 379);
 			this.guna2TextBox1.Name = "guna2TextBox1";
 			this.guna2TextBox1.PasswordChar = '\0';
 			this.guna2TextBox1.PlaceholderText = "";
 			this.guna2TextBox1.SelectedText = "";
 			this.guna2TextBox1.ShadowDecoration.Parent = this.guna2TextBox1;
-			this.guna2TextBox1.Size = new System.Drawing.Size(596, 96);
+			this.guna2TextBox1.Size = new System.Drawing.Size(468, 96);
 			this.guna2TextBox1.TabIndex = 48;
-			// 
-			// guna2DateTimePicker2
-			// 
-			this.guna2DateTimePicker2.BorderRadius = 8;
-			this.guna2DateTimePicker2.Checked = true;
-			this.guna2DateTimePicker2.CheckedState.Parent = this.guna2DateTimePicker2;
-			this.guna2DateTimePicker2.FillColor = System.Drawing.Color.White;
-			this.guna2DateTimePicker2.Font = new System.Drawing.Font("Segoe UI", 9F);
-			this.guna2DateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Long;
-			this.guna2DateTimePicker2.HoverState.Parent = this.guna2DateTimePicker2;
-			this.guna2DateTimePicker2.Location = new System.Drawing.Point(37, 133);
-			this.guna2DateTimePicker2.MaxDate = new System.DateTime(9998, 12, 31, 0, 0, 0, 0);
-			this.guna2DateTimePicker2.MinDate = new System.DateTime(1753, 1, 1, 0, 0, 0, 0);
-			this.guna2DateTimePicker2.Name = "guna2DateTimePicker2";
-			this.guna2DateTimePicker2.ShadowDecoration.Parent = this.guna2DateTimePicker2;
-			this.guna2DateTimePicker2.Size = new System.Drawing.Size(230, 43);
-			this.guna2DateTimePicker2.TabIndex = 47;
-			this.guna2DateTimePicker2.Value = new System.DateTime(2021, 12, 8, 8, 21, 48, 501);
-			// 
-			// guna2DateTimePicker1
-			// 
-			this.guna2DateTimePicker1.BorderRadius = 8;
-			this.guna2DateTimePicker1.Checked = true;
-			this.guna2DateTimePicker1.CheckedState.Parent = this.guna2DateTimePicker1;
-			this.guna2DateTimePicker1.CustomFormat = "hh:mm";
-			this.guna2DateTimePicker1.FillColor = System.Drawing.Color.White;
-			this.guna2DateTimePicker1.Font = new System.Drawing.Font("Segoe UI", 9F);
-			this.guna2DateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-			this.guna2DateTimePicker1.HoverState.Parent = this.guna2DateTimePicker1;
-			this.guna2DateTimePicker1.Location = new System.Drawing.Point(273, 133);
-			this.guna2DateTimePicker1.MaxDate = new System.DateTime(9998, 12, 31, 0, 0, 0, 0);
-			this.guna2DateTimePicker1.MinDate = new System.DateTime(1753, 1, 1, 0, 0, 0, 0);
-			this.guna2DateTimePicker1.Name = "guna2DateTimePicker1";
-			this.guna2DateTimePicker1.ShadowDecoration.Parent = this.guna2DateTimePicker1;
-			this.guna2DateTimePicker1.ShowUpDown = true;
-			this.guna2DateTimePicker1.Size = new System.Drawing.Size(134, 43);
-			this.guna2DateTimePicker1.TabIndex = 46;
-			this.guna2DateTimePicker1.Value = new System.DateTime(2021, 12, 8, 8, 37, 11, 123);
 			// 
 			// label5
 			// 
 			this.label5.AutoSize = true;
 			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label5.Location = new System.Drawing.Point(358, 303);
+			this.label5.Location = new System.Drawing.Point(33, 276);
 			this.label5.Name = "label5";
 			this.label5.Size = new System.Drawing.Size(61, 20);
 			this.label5.TabIndex = 45;
@@ -368,7 +290,7 @@ namespace Schedule_Job
 			// 
 			this.label4.AutoSize = true;
 			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label4.Location = new System.Drawing.Point(41, 304);
+			this.label4.Location = new System.Drawing.Point(283, 276);
 			this.label4.Name = "label4";
 			this.label4.Size = new System.Drawing.Size(113, 20);
 			this.label4.TabIndex = 44;
@@ -378,7 +300,7 @@ namespace Schedule_Job
 			// 
 			this.label3.AutoSize = true;
 			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label3.Location = new System.Drawing.Point(33, 203);
+			this.label3.Location = new System.Drawing.Point(31, 194);
 			this.label3.Name = "label3";
 			this.label3.Size = new System.Drawing.Size(211, 20);
 			this.label3.TabIndex = 43;
@@ -388,7 +310,7 @@ namespace Schedule_Job
 			// 
 			this.label2.AutoSize = true;
 			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label2.Location = new System.Drawing.Point(33, 110);
+			this.label2.Location = new System.Drawing.Point(33, 118);
 			this.label2.Name = "label2";
 			this.label2.Size = new System.Drawing.Size(213, 20);
 			this.label2.TabIndex = 42;
@@ -404,11 +326,59 @@ namespace Schedule_Job
 			this.label1.TabIndex = 41;
 			this.label1.Text = "Tên chi tiết công việc:";
 			// 
+			// guna2TextBox4
+			// 
+			this.guna2TextBox4.Cursor = System.Windows.Forms.Cursors.IBeam;
+			this.guna2TextBox4.DefaultText = "";
+			this.guna2TextBox4.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+			this.guna2TextBox4.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+			this.guna2TextBox4.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+			this.guna2TextBox4.DisabledState.Parent = this.guna2TextBox4;
+			this.guna2TextBox4.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+			this.guna2TextBox4.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+			this.guna2TextBox4.FocusedState.Parent = this.guna2TextBox4;
+			this.guna2TextBox4.Font = new System.Drawing.Font("Segoe UI", 9F);
+			this.guna2TextBox4.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+			this.guna2TextBox4.HoverState.Parent = this.guna2TextBox4;
+			this.guna2TextBox4.Location = new System.Drawing.Point(37, 141);
+			this.guna2TextBox4.Name = "guna2TextBox4";
+			this.guna2TextBox4.PasswordChar = '\0';
+			this.guna2TextBox4.PlaceholderText = "";
+			this.guna2TextBox4.SelectedText = "";
+			this.guna2TextBox4.ShadowDecoration.Parent = this.guna2TextBox4;
+			this.guna2TextBox4.Size = new System.Drawing.Size(109, 36);
+			this.guna2TextBox4.TabIndex = 61;
+			// 
+			// guna2TextBox5
+			// 
+			this.guna2TextBox5.Cursor = System.Windows.Forms.Cursors.IBeam;
+			this.guna2TextBox5.DefaultText = "";
+			this.guna2TextBox5.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+			this.guna2TextBox5.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+			this.guna2TextBox5.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+			this.guna2TextBox5.DisabledState.Parent = this.guna2TextBox5;
+			this.guna2TextBox5.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+			this.guna2TextBox5.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+			this.guna2TextBox5.FocusedState.Parent = this.guna2TextBox5;
+			this.guna2TextBox5.Font = new System.Drawing.Font("Segoe UI", 9F);
+			this.guna2TextBox5.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+			this.guna2TextBox5.HoverState.Parent = this.guna2TextBox5;
+			this.guna2TextBox5.Location = new System.Drawing.Point(35, 217);
+			this.guna2TextBox5.Name = "guna2TextBox5";
+			this.guna2TextBox5.PasswordChar = '\0';
+			this.guna2TextBox5.PlaceholderText = "";
+			this.guna2TextBox5.SelectedText = "";
+			this.guna2TextBox5.ShadowDecoration.Parent = this.guna2TextBox5;
+			this.guna2TextBox5.Size = new System.Drawing.Size(111, 36);
+			this.guna2TextBox5.TabIndex = 62;
+			// 
 			// AddjobDetailFrm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(676, 573);
+			this.ClientSize = new System.Drawing.Size(544, 573);
+			this.Controls.Add(this.guna2TextBox5);
+			this.Controls.Add(this.guna2TextBox4);
 			this.Controls.Add(this.guna2TextBox3);
 			this.Controls.Add(this.label6);
 			this.Controls.Add(this.btnCancel);
@@ -416,11 +386,7 @@ namespace Schedule_Job
 			this.Controls.Add(this.groupBox1);
 			this.Controls.Add(this.guna2ComboBox1);
 			this.Controls.Add(this.guna2TextBox2);
-			this.Controls.Add(this.guna2DateTimePicker4);
-			this.Controls.Add(this.guna2DateTimePicker3);
 			this.Controls.Add(this.guna2TextBox1);
-			this.Controls.Add(this.guna2DateTimePicker2);
-			this.Controls.Add(this.guna2DateTimePicker1);
 			this.Controls.Add(this.label5);
 			this.Controls.Add(this.label4);
 			this.Controls.Add(this.label3);
@@ -448,15 +414,13 @@ namespace Schedule_Job
 		private Guna.UI2.WinForms.Guna2CheckBox guna2CheckBox3;
 		private Guna.UI2.WinForms.Guna2ComboBox guna2ComboBox1;
 		private Guna.UI2.WinForms.Guna2TextBox guna2TextBox2;
-		private Guna.UI2.WinForms.Guna2DateTimePicker guna2DateTimePicker4;
-		private Guna.UI2.WinForms.Guna2DateTimePicker guna2DateTimePicker3;
 		private Guna.UI2.WinForms.Guna2TextBox guna2TextBox1;
-		private Guna.UI2.WinForms.Guna2DateTimePicker guna2DateTimePicker2;
-		private Guna.UI2.WinForms.Guna2DateTimePicker guna2DateTimePicker1;
 		private System.Windows.Forms.Label label5;
 		private System.Windows.Forms.Label label4;
 		private System.Windows.Forms.Label label3;
 		private System.Windows.Forms.Label label2;
 		private System.Windows.Forms.Label label1;
+		private Guna.UI2.WinForms.Guna2TextBox guna2TextBox4;
+		private Guna.UI2.WinForms.Guna2TextBox guna2TextBox5;
 	}
 }
